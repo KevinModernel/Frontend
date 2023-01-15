cfc9f26c418a9b88ae6a4bf602421ce98fffe143
KevinModernel <modernelkevin96@gmail.com>
2023-01-14 23:31:06 -0300

    Frontend for ML APP.
    FrontEnd working, it takes data from MOCK_DATA.
    It needs connection between front and back, and sending the .json from the back to the front.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import { SortingTable } from './components/SortingTable';
+//import { index } from './components/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+	const [inputs, setInputs] = useState({});
+
+
+	const handleChange = (event) => {
+    	const marca = event.target.marca;
+    	const modelo = event.target.modelo;
+    	setInputs(values => ({...values, [marca]: modelo}))
+	}
+
+ 	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>ML app - Análisis de precios</h1>
+			</header>
+
+			<body>
+				<form action="/listado" method="post" className="form">
+					<div>
+						<label>Ingresar Marca:
+						<input type="text" name='marca' value={inputs.username} onChange={handleChange} />
+						</label>
+						<label>Ingresar Modelo:
+                		<input type="text" name='modelo' value={inputs.username} onChange={handleChange} />
+                		</label>
+					</div>
+					<button type="submit"> Consultar </button>
+				</form>	
+					<SortingTable />		
+			</body>
+
+		</div>
+	);
+}
+
+
+export default App;
+
+// Learn LINK.
+// 
+
+
+
+
+/*        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,9 +56,28 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+*/
 
-export default App;
+
+
+
+/*
+const [data setData] = React.useState(null);
+
+React.useEffect(() => {
+	fetch("/api")
+ 		.then((res) => res.json())
+ 		.then((data) => setData(data.message));
+	} []
+);
+
+
+
+function App() {
+	return (
+		<div className="App">
+			<SortingTable />
+		</div>
+	);
+}
+*/
